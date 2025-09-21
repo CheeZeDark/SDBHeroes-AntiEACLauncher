b2aa6a9c0c565d58f0b736fbbac12dc9b8bb7e42
CheeZe Dark(Rikko Matsumato) <110032482+CheeZeDark@users.noreply.github.com>
2025-09-21 18:04:23 +0300

    Update Main.py

--- SDBH_WorldMission_AntiEACCopier/Main.py
+++ SDBH_WorldMission_AntiEACCopier/Main.py
@@ -13,13 +13,13 @@ def Main():
         print("Real Size of Fake Launcher of EAC(EasyAntiCheat): {}".format(PE_DragonBallHeroes.OPTIONAL_HEADER.SizeOfImage))
         input_yes = input("Do you Want to Copy This Fake EAC Launcher(Super Dragon Ball Heroes)?")
         if(input_yes == "y"):
-            shutil.copy2("C:\\Program Files (x86)\\Steam\\steamapps\\common\\SUPER DRAGON BALL HEROES WORLD MISSION\\Launch_Game.exe", n.getcwd() + "Launch_Game_TMP.exe")
+            shutil.copy2("C:\\Program Files (x86)\\Steam\\steamapps\\common\\SUPER DRAGON BALL HEROES WORLD MISSION\\Launch_Game.exe", "{}".format(n.getcwd() + "\\Launch_Game_TMP.exe"))
             print("PLS DO NOT TOUCH Launch_Game_TMP.exe(IN TEMP FOLDER) \nIF YOU WANTED, YOU CAN RENAME THIS TO LAUNCH_GAME.EXE AND COPY TO LICENSE FOLDER OF SUPER DRAGON BALL HEROES TO RETURN ONLINE MULTIPLAYER!!!")
             shutil.copy2(dragonballheroes_realfile, "C:\\Program Files (x86)\\Steam\\steamapps\\common\\SUPER DRAGON BALL HEROES WORLD MISSION\\Launch_Game.exe")
             print("Successfully Copied!!!")
-            tmp_reallauncher_superdragonballheroes = n.getenv['TEMP'] + "\\Launch_Game.exe"
-            shutil.copy2("Launch_Game_TMP.exe", tmp_reallauncher_superdragonballheroes)
+            shutil.copy2("Launch_Game_TMP.exe", "{}".format(n.getenv("TEMP") + "\\Launch_Game.exe")) # os.getenv["VARIABLE_NAME"] outdated in Python 3.10 or lower.
             n.remove("Launch_Game_TMP.exe")
+            n.remove("Launch_Game.exe")
             print("Successfully Bypassed EAC Protected Game Super Dragon Ball Heroes!!! \nCreated By Rikko Matsumato!!!")
             exit(3112)
         else:
